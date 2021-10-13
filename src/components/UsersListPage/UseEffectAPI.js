be2f6fc824406bd941b1039d011adda3f8403677
Aneta <a.banas.brejt@gmail.com>
2021-10-13 21:37:58 +0200

    Connect API  https://api.github.com/users working in console

--- src/components/UsersListPage/UseEffectAPI.js
+++ src/components/UsersListPage/UseEffectAPI.js
@@ -1,9 +1,22 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import React from 'react';
+
 import './usersListPage.css';
 import { FaGithub } from 'react-icons/fa';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState, useEffect } from 'react';
 
 const UseEffectAPI = () => {
+  const getUsers = async () => {
+    // eslint-disable-next-line no-undef
+    const response = await fetch('https://api.github.com/users');
+    const data = await response.json();
+    // eslint-disable-next-line no-undef
+    console.log(data);
+  };
+  // eslint-disable-next-line no-undef
+  useEffect(() => {
+    getUsers();
+  });
   return (
     <div>
       <h1>
@@ -34,64 +47,6 @@ const UseEffectAPI = () => {
                     <div className='d-flex flex-column'>
                       <span className='number3'>Link</span>{' '}
                       {/*here will be button to users profile repos_url*/}
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className='col-10 col md-4 mt-5'>
-                <div class='card p-2'>
-                  <div class='d-flex align-items-center'>
-                    <div class='image'>
-                      <img class='rounded' width='155'></img>{' '}
-                      {/* img src={avatar_url} is for Avatar injection*/}
-                      <div class='ml-3 w-100'>
-                        <h4 className='mb-0 mt-0 textLeft'>Aneta</h4>{' '}
-                        {/*h4 is for username/login injection*/}
-                        <span className='textLeft'>Maybe Metapack</span>
-                        <div className='p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats'></div>
-                        <div className='d-flex flex-column'>
-                          <span className='number1'>id</span>{' '}
-                          {/*here will be id injection*/}
-                        </div>
-                        <div className='d-flex flex-column'>
-                          <span className='number2'>Team</span>{' '}
-                          {/*here will be budge inserted*/}
-                        </div>
-                        <div className='d-flex flex-column'>
-                          <span className='number3'>Link</span>{' '}
-                          {/*here will be button to users profile repos_url*/}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className='col-10 col md-4 mt-5'>
-                <div class='card p-2'>
-                  <div class='d-flex align-items-center'>
-                    <div class='image'>
-                      <img class='rounded' width='155'></img>{' '}
-                      {/* img src={avatar_url} is for Avatar injection*/}
-                      <div class='ml-3 w-100'>
-                        <h4 className='mb-0 mt-0 textLeft'>Aneta</h4>{' '}
-                        {/*h4 is for username/login injection*/}
-                        <span className='textLeft'>Maybe Metapack</span>
-                        <div className='p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats'></div>
-                        <div className='d-flex flex-column'>
-                          <span className='number1'>id</span>{' '}
-                          {/*here will be id injection*/}
-                        </div>
-                        <div className='d-flex flex-column'>
-                          <span className='number2'>Tram</span>{' '}
-                          {/*here will be budge inserted*/}
-                        </div>
-                        <div className='d-flex flex-column'>
-                          <span className='number3'>Link</span>{' '}
-                          {/*here will be button to users profile repos_url*/}
-                        </div>
-                      </div>
                     </div>
                   </div>
                 </div>
