6c6ed06cf21aa6db0f10769ac40c6f2a265109c9
Aneta <a.banas.brejt@gmail.com>
2021-10-14 18:06:14 +0200

    GithubAPI implementation live. All data visible. Adjust visual to mockup - basic.

--- src/components/UsersListPage/UseEffectAPI.js
+++ src/components/UsersListPage/UseEffectAPI.js
@@ -6,17 +6,18 @@ import { FaGithub } from 'react-icons/fa';
 import React, { useState, useEffect } from 'react';
 
 const UseEffectAPI = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [users, setUsers] = useState([]);
+
   const getUsers = async () => {
     // eslint-disable-next-line no-undef
     const response = await fetch('https://api.github.com/users');
-    const data = await response.json();
-    // eslint-disable-next-line no-undef
-    console.log(data);
+    setUsers(await response.json());
   };
   // eslint-disable-next-line no-undef
   useEffect(() => {
     getUsers();
-  });
+  }, []);
   return (
     <div>
       <h1>
@@ -25,34 +26,48 @@ const UseEffectAPI = () => {
       </h1>
       <div className='container-fluid mt-5'>
         <div className='row text-center'>
-          <div className='col-10 col md-4 mt-5'>
-            <div class='card p-2'>
-              <div class='d-flex align-items-center'>
-                <div class='image'>
-                  <img class='rounded' width='155'></img>{' '}
-                  {/* img src={avatar_url} is for Avatar injection*/}
-                  <div class='ml-3 w-100'>
-                    <h4 className='mb-0 mt-0 textLeft'>Aneta</h4>{' '}
-                    {/*h4 is for username/login injection*/}
-                    <span className='textLeft'>Maybe Metapack</span>
-                    <div className='p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats'></div>
-                    <div className='d-flex flex-column'>
-                      <span className='number1'>id</span>{' '}
-                      {/*here will be id injection*/}
-                    </div>
-                    <div className='d-flex flex-column'>
-                      <span className='number2'>Team</span>{' '}
-                      {/*here will be budge inserted*/}
-                    </div>
-                    <div className='d-flex flex-column'>
-                      <span className='number3'>Link</span>{' '}
-                      {/*here will be button to users profile repos_url*/}
+          {users.map((curElem) => {
+            const { avatar_url, id, login, repos_url } = curElem;
+            return (
+              <div className='col-10 col md-4 mt-5'>
+                <div class='card p-2'>
+                  <div class='d-flex align-items-center'>
+                    <div class='image'>
+                      <img
+                        src={avatar_url}
+                        className='rounded'
+                        width='155'
+                      ></img>{' '}
+                      {/* img src={avatar_url} is for Avatar injection*/}
+                      <div class='ml-3 w-100'>
+                        <h4 className='mb-0 mt-0 textLeft'>@{curElem.login}</h4>{' '}
+                        {/*h4 is for username/login injection*/}
+                        <div className='d-flex flex-column'>
+                          <span className='number1'>ID:#{id}</span>{' '}
+                          {/*here will be id injection*/}
+                        </div>
+                        <div className='d-flex flex-column'>
+                          <span className='number2'>Team</span>{' '}
+                          {/*here will be budge inserted*/}
+                        </div>
+                        <div className='d-flex flex-column'>
+                          <span className='number3'>
+                            <FaGithub />
+                            GitHub page
+                          </span>{' '}
+                          {/*here will be button to users profile repos_url*/}
+                          <div className='d-flex flex-column'>
+                            <span className='number2'>Details</span>{' '}
+                            {/*here will be details link*/}
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
